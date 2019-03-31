c941ff4ca4910a3a74ddc5e29ff2898438711377
Reimar Stier <mail@reimarstier.de>
2019-03-31 19:59:55 +0200

    Add kafka config tests

--- docs/Kafka.md
+++ docs/Kafka.md
@@ -3,6 +3,11 @@ https://hub.docker.com/r/confluentinc/cp-kafka
 https://rmoff.net/2018/08/02/kafka-listeners-explained/
 https://docs.confluent.io/current/installation/docker/image-reference.html#image-reference
 
+# Kafka Testenvironment
+Addresses:
+* `kafka:9092` within the docker network
+* `localhost:9093` from docker host machine
+
 # Kafka useful commands
 kafka-console-producer --broker-list kafka:9092 --topic test
 kafka-topics --zookeeper zookeeper --list
--- src/test/python/test_kafka_up.py
+++ src/test/python/test_kafka_up.py
@@ -1,8 +1,8 @@
 import unittest
 
 import hamcrest
-from confluent_kafka.admin import AdminClient
-from confluent_kafka.cimpl import NewTopic
+from confluent_kafka.admin import AdminClient, ConfigResource, ConfigSource
+from confluent_kafka.cimpl import NewTopic, RESOURCE_TOPIC, KafkaException
 from hamcrest import assert_that, equal_to, contains_string
 
 
@@ -14,6 +14,7 @@ class Topic:
 class TestImport(unittest.TestCase):
     def setUp(self):
         self.broker = "kafka:9092"
+        # self.broker = "localhost:9093"
         self.admin_client = AdminClient({'bootstrap.servers': self.broker})
 
     def test_000_it(self):
@@ -25,7 +26,7 @@ class TestImport(unittest.TestCase):
 
         result = self.admin_client.list_topics(timeout=90)
 
-        assert_that(result.orig_broker_name, equal_to("kafka:9092/1"))
+        assert_that(result.orig_broker_name, equal_to("%s/1" % self.broker))
 
     def test_002_test_create_topic(self):
 
@@ -47,7 +48,7 @@ class TestImport(unittest.TestCase):
             "retention.bytes": 10000,
             "retention.ms": 3600,
         }
-        new_topics = [NewTopic("testrun2", 1, 1, config=config)]
+        new_topics = [NewTopic("test_config", 1, 1, config=config)]
 
         result_futures = self.admin_client.create_topics(new_topics=new_topics)
 
@@ -57,3 +58,28 @@ class TestImport(unittest.TestCase):
                 self.assertTrue(True)
             except Exception as e:
                 assert_that(str(e), contains_string("already exists."))
+
+    def test_get_config(self):
+        config_resource = [ConfigResource(RESOURCE_TOPIC, "test_config")]
+        futures = self.admin_client.describe_configs(config_resource)
+        for res, f in futures.items():
+            try:
+                configs = f.result()
+
+                assert_that(configs["retention.ms"].value, equal_to("3600"))
+                assert_that(configs["retention.bytes"].value, equal_to("10000"))
+                assert_that(configs["delete.retention.ms"].value,
+                            equal_to("3600"))
+
+            except KafkaException as e:
+                raise
+            except Exception:
+                raise
+
+    def test_delete_topics(self):
+        topics = ["testrun", "testrun2"]
+        futures = self.admin_client.delete_topics(topics, operation_timeout=30)
+
+        for topic, f in futures.items():
+            result = f.result()
+            self.assertIsNone(result)
